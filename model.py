69e63121a36021def393c5d824f3d14176df549b
Omer Ulucan <omer.ulucan80@gmail.com>
2026-06-04 22:33:57 -0500

    added model download function with lazy loading

---
+++ model.py
@@ -0,0 +1,16 @@
+from llama_cpp import Llama
+
+_llm = None
+
+def get_model():
+    global _llm
+    if _llm is None:
+        _llm = Llama.from_pretrained(
+            repo_id="bartowski/Qwen3.5-2B-GGUF",
+            filename="*Q4_K_M.gguf",
+            verbose=False,
+            n_ctx=8192,
+            chat_format="chatml",
+        )
+    
+    return _llm
