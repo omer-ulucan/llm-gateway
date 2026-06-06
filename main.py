06867fa577caa94dd80bae1730fa7a04916fd684
Omer Ulucan <omer.ulucan80@gmail.com>
2026-06-05 23:38:21 -0500

    added mlflow tracing and evaluation metrics

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from fastapi import FastAPI, HTTPException
 from schemas import ChatRequest, ChatResponse
 from model import get_model
+import mlflow
+import time
+
+
+mlflow.set_tracking_uri("https://localhost:5000")
+mlflow.set_experiment("llm-gateway")
+
 
 app = FastAPI()
 
 @app.post("/v1/chat/completions")
+@mlflow.trace
 async def llm_communication(request: ChatRequest):
     llm = get_model()
+    start = time.time()
+
     result = llm.create_chat_completion(
         messages=[
             {"role": m.role, "content": m.content} for m in request.messages
             ]
         )
     
+    content = result["choices"][0]["message"]["content"]  
+
+    latency = time.time() - start
+    token_count = result["usage"]["total_tokens"]
+    response_length = len(result["choices"][0]["message"]["content"])
+
+    mlflow.log_metric("latency", latency)
+    mlflow.log_metric("token_count", token_count)
+    mlflow.log_metric("response_length", response_length)
+    
     return ChatResponse(
         model=request.model,
-        content=result["choices"][0]["message"]["content"]   
+        content=content
     )
